7b3692d219169d304f2df2104b06852974e41d9b
Bruno Freire <brunofreire1717@gmail.com>
2023-02-26 10:12:59 -0500

    balanceAndTexteContainerUpdate

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let transactions = []
+const formarter = Intl.NumberFormat('pt-BR', {
+  compactDisplay: 'long',
+  style: 'currency',
+  currency: 'BRL'
+})
 
 function createTransactionContainer(id){
   const container = document.createElement('div')
@@ -18,27 +23,38 @@ function createTransactionAmount(amount){
   const span = document.createElement('span')
   span.classList.add('transaction-amount')
   
-  const formarter = Intl.NumberFormat('pt-BR', {
-    compactDisplay: 'long',
-    style: 'currency',
-    currency: 'BRL'
-  })
   const formartedAmount = formarter.format(amount)
-  span.textContent = formartedAmount
+
+  if(amount > 0 ){
+    span.classList.add('credit')
+    span.textContent = `${formartedAmount} C`
+  }else{
+    span.classList.add('debit')
+    span.textContent = `${formartedAmount} D`
+  }
   return span
+}
+
+function updateBalance(){
+  const balance = document.getElementById('balance')
+  const fullAmount = transactions.reduce((sum, transaction) => sum + transaction.amount, 0)
+
+  const formartedAmount = formarter.format(fullAmount)
+  balance.textContent = formartedAmount
 }
 
 async function saveTransaction(ev){
   ev.preventDefault()
-  
-  const name = document.getElementById('name').value
-  const amount = document.getElementById('amount').value
 
-  const result = await fetch('http://localhost:3000/transactions', {
+  const name = document.getElementById('name').value
+  const amount = parseFloat(document.getElementById('amount').value)
+
+  await fetch('http://localhost:3000/transactions', {
     method: 'POST',
     body: JSON.stringify({name,amount}),
     headers: {'Content-Type': 'application/json'}
   })
+  updateBalance()
 }
 
 function renderTransaction(transaction){
@@ -49,6 +65,7 @@ function renderTransaction(transaction){
   const section = document.getElementById('transactions')
   container.append(title, amount)
   section.append(container)
+  updateBalance()
 }
 
 async function fetchTransactions(){
